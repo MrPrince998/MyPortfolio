512d4dced74382263d0a3ac1ad6af61e078cb2d6
MrPrince998 <peincerana234@gmail.com>
2025-06-09 21:40:02 +0545

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Theme } from "@radix-ui/themes/dist/cjs/index.js";
 import { AuthProvider } from "./context/AuthProvider.jsx";
+import { Theme } from "@radix-ui/themes";
 
 export const queryClient = new QueryClient();
 
